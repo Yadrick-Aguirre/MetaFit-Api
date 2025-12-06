35ecadc80373eda2681839ae65705f01705b26d7
Yadrick-Aguirre <yadrick92@gmail.com>
2025-12-06 00:20:32 -0600

    The API created in the proyect is implemeted

--- app/src/main/java/Network/RetrofitClient.kt
+++ app/src/main/java/Network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://metafit-api.onrender.com" // TU URL
+    private const val BASE_URL = "https://metafit-api.onrender.com" // URL DE MI RENDER
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
